49f80d46a85e05d9f92083c8da3c13959fb70dbf
Devanshu <alphatechnologies8005@gmail.com>
2021-09-22 11:50:06 +0100

    ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql')
+var mongoose=require("mongoose");
 const express=require("express");
 const app=express()
 const path=require("path")
@@ -8,37 +8,38 @@ app.use(express.static(path.join(__dirname,"static")))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'techwith_saksham',
-  password: 'Saksham@4672',
-  database: 'techwith_sakshamdatabase'
+mongoose.connect("mongodb+srv://user-sd:Saksham4672@cluster0.d28bj.mongodb.net/project?retryWrites=true&w=majority").then(()=>{
+  console.log("Connected to mongdb!")
+}).catch((err)=>{
+  console.log("not connected to mongodb!")
 })
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log('Database is connected successfully !');
+var userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: "This field is required.",
+  },
+  lastname: {
+    type: String,
+  },
 });
+
+var User= mongoose.model("User", userSchema);
+
 
 app.get("/",(req,res)=>{
 res.sendFile(path.join(__dirname,"index.html"))
 })
 
-app.post("/save.html",(req,res)=>{
-  var firstname=req.body.firstname;
-  var lastname=req.body.lastname;
-
-  var sql="insert into saksham2 values('"+firstname+"','"+lastname+"')"
-  connection.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log('record inserted');
-    console.log(sql);
-    res.redirect('/');
-  });
-
+app.post("/",(req,res)=>{
+ var user=new User();
+ user.firstname = req.body.firstname;
+ user.lastname = req.body.lastname;
+ user.save();
+ res.send(user);
+ res.redirect("/");
 })
+ 
 
 app.listen(port,(req,res)=>{
    console.log("connected to server!")
